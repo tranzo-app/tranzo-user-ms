bfe3d1cb916153c422c7219e864db26bc3c129de
JahnaviSatishShanbhag <jahnavisatish.cs19@bmsce.ac.in>
2025-12-11 20:08:25 +0530

    corrected h2 database configurations

--- src/main/java/com/tranzo/tranzo_user_ms/repository/UserRepository.java
+++ src/main/java/com/tranzo/tranzo_user_ms/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<UsersEntity, UUID> {
     Optional<UsersEntity> findUserByUserUuid(UUID userUuid);
 }
