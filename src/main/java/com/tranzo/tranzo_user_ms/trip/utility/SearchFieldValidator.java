5a50ffa6dc7c843f98ad673496afe531ecd8e3fb
JahnaviSatishShanbhag <jahnavisatish.cs19@bmsce.ac.in>
2026-03-10 00:02:23 +0530

    corrected search api

--- src/main/java/com/tranzo/tranzo_user_ms/trip/utility/SearchFieldValidator.java
+++ src/main/java/com/tranzo/tranzo_user_ms/trip/utility/SearchFieldValidator.java
@@ -1,25 +1,35 @@
 package com.tranzo.tranzo_user_ms.trip.utility;
 
+import com.tranzo.tranzo_user_ms.trip.model.TripEntity;
+
 import java.time.LocalDate;
 import java.util.Map;
 
 public class SearchFieldValidator {
-    private static final Map<String, Class<?>> ALLOWED_FIELDS = Map.of(
-            "tripDestination", String.class,
-            "tripStartDate", String.class,
-            "tripEndDate", String.class
-    );
+//    private static final Map<String, Class<?>> ALLOWED_FIELDS = Map.of(
+//            "tripDestination", String.class,
+//            "tripStartDate", String.class,
+//            "tripEndDate", String.class
+//    );
 
     public static void validate(String field) {
 
-        if (!ALLOWED_FIELDS.containsKey(field)) {
-            throw new IllegalArgumentException(
-                    "Filtering not allowed on field: " + field
-            );
-        }
+//        if (!ALLOWED_FIELDS.containsKey(field)) {
+//            throw new IllegalArgumentException(
+//                    "Filtering not allowed on field: " + field
+//            );
+//        }
     }
 
     public static Class<?> getFieldType(String field) {
-        return ALLOWED_FIELDS.get(field);
+        try {
+            return TripEntity.class.getDeclaredField(field).getType();
+        } catch (Exception e) {
+            throw new RuntimeException("Invalid field: " + field);
+        }
     }
+
+//    public static Class<?> getFieldType(String field) {
+//        return ALLOWED_FIELDS.get(field);
+//    }
 }
