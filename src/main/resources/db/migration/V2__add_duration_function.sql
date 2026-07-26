f733950e93aaabac427a099c6b1fa194d882fc7f
Jahnavi S S <jahnaviss44@gmail.com>
2026-07-27 00:07:41 +0530

    fixed new search endpoint

--- src/main/resources/db/migration/V2__add_duration_function.sql
+++ src/main/resources/db/migration/V2__add_duration_function.sql
@@ -1,8 +1,9 @@
 -- Add custom function to calculate trip duration in days
 -- This function provides a reliable way to calculate duration between two dates
+-- PostgreSQL DATE subtraction returns integer days directly
 CREATE OR REPLACE FUNCTION calculate_trip_duration(end_date DATE, start_date DATE)
 RETURNS INTEGER AS $$
 BEGIN
-    RETURN EXTRACT(DAY FROM (end_date - start_date));
+    RETURN (end_date - start_date);
 END;
 $$ LANGUAGE plpgsql IMMUTABLE;
